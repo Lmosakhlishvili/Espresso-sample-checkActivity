b6eb66e076d238f670e3be2e54be60f1416a29c5
DataShalam <d.shalamberidze04@gmail.com>
2023-11-30 00:32:04 +0400

    Completed HW Hello_Espresso

--- app/src/androidTest/java/com/ghedamsisabri/ui_testing_for_beginner/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ghedamsisabri/ui_testing_for_beginner/ExampleInstrumentedTest.kt
@@ -5,6 +5,9 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.ghedamsisabri.ui_testing_for_beginner.Helper.assertIfViewIsNotDisplayed
+import com.ghedamsisabri.ui_testing_for_beginner.Helper.assertIsNotViewDisplayed
+import com.ghedamsisabri.ui_testing_for_beginner.Helper.assertIsViewDisplayed
 import com.ghedamsisabri.ui_testing_for_beginner.Helper.getText
 import com.ghedamsisabri.ui_testing_for_beginner.Helper.isTextOnScreen
 import com.ghedamsisabri.ui_testing_for_beginner.Helper.isViewDisplayed
@@ -33,8 +36,36 @@ class ExampleInstrumentedTest {
         Assert.assertEquals("SecondaryActivity", secondPageActivity.getText(5))
     }
 
+    @Test
+    fun validateMainAndSecondaryScreens() {
+        MainPageActivity.assertIsViewDisplayed()
+        NextBtn.assertIsViewDisplayed()
+
+        NextBtn.tap()
+
+        secondPageActivity.assertIsViewDisplayed()
+        Assert.assertEquals("SecondaryActivity", secondPageActivity.getText())
+
+        BackBtn.assertIsViewDisplayed()
+        Assert.assertEquals("Back", BackBtn.getText())
+    }
+
+    @Test
+    fun validateNavigationInAPP() {
+        MainPageActivity.assertIsViewDisplayed()
+        NextBtn.assertIsViewDisplayed()
+
+        NextBtn.tap()
+        BackBtn.tap()
+
+        secondPageActivity.assertIsNotViewDisplayed()
+        MainPageActivity.assertIsViewDisplayed()
+    }
+
     companion object{
         val NextBtn: Matcher<View> by lazy { withId(R.id.button_next_activity) }
         val secondPageActivity: Matcher<View> by lazy { withId(R.id.activity_secondary_title) }
+        val MainPageActivity: Matcher<View> by lazy { withId(R.id.activity_main_title) }
+        val BackBtn: Matcher<View> by lazy { withId(R.id.button_back) }
     }
 }
